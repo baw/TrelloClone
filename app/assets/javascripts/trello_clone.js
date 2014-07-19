fe0e8581154540b316df04c19a3abac7498097c4
Brian <iamamthwiz@gmail.com>
2014-07-18 18:31:53 -0700

    add app initilization

--- app/assets/javascripts/trello_clone.js
+++ app/assets/javascripts/trello_clone.js
@@ -1,8 +1,18 @@
+/*global TrelloClone */
 window.TrelloClone = {
   Models: {},
   Collections: {},
   Views: {},
   Routers: {},
   initialize: function() {
+    TrelloClone.Collections.boards = new TrelloClone.Collections.Boards();
+    TrelloClone.Collections.cards  = new TrelloClone.Collections.Cards();
+    TrelloClone.Collections.lists  = new TrelloClone.Collections.Lists();
+    
+    var routerOptions = {
+      mainAreaSelector: "#main"
+    };
+    new TrelloClone.Routers.Router(routerOptions);
+    Backbone.history.start();
   }
 };
