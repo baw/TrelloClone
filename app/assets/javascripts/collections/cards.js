e294c9b0a478e04cf57c0bf77018536290110d20
Brian <iamamthwiz@gmail.com>
2014-07-21 00:14:22 -0700

    add ordering of cards

--- app/assets/javascripts/collections/cards.js
+++ app/assets/javascripts/collections/cards.js
@@ -1,5 +1,10 @@
 /*global TrelloClone */
 TrelloClone.Collections.Cards = Backbone.Collection.extend({
   model: TrelloClone.Models.Card,
-  url: "api/cards"
+  url: "api/cards",
+  
+  comparator: function (model) {
+    console.log(model.escape("ord"));
+    return model.escape("ord");
+  }
 });
